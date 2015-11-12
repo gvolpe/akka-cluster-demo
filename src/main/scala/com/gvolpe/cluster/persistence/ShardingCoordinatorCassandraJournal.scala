5d368a722eca86fa8177041f60bb2a140cb2066f
Gabi Volpe <gabriel.volpe@paddypower.com>
2015-11-12 12:11:33 +0000

    Cassandra plugin extension working with default properties taken from default values using withFallback.

--- src/main/scala/com/gvolpe/cluster/persistence/ShardingCoordinatorCassandraJournal.scala
+++ src/main/scala/com/gvolpe/cluster/persistence/ShardingCoordinatorCassandraJournal.scala
@@ -2,9 +2,11 @@ package com.gvolpe.cluster.persistence
 
 import akka.persistence.cassandra.PluginConfiguration
 import akka.persistence.cassandra.journal.CassandraJournal
+import com.typesafe.config.Config
 
 class ShardingCoordinatorCassandraJournal extends CassandraJournal with ShardingJournalPluginConfiguration
 
 trait ShardingJournalPluginConfiguration extends PluginConfiguration {
-  override def configurationKey: String = "sharding-journal"
+  override def pluginConfig(systemConfig: Config): Config = systemConfig.getConfig("sharding-journal")
+    .withFallback(systemConfig.getConfig("cassandra-journal"))
 }
--- src/main/scala/com/gvolpe/cluster/persistence/ShardingCoordinatorCassandraSnapshot.scala
+++ src/main/scala/com/gvolpe/cluster/persistence/ShardingCoordinatorCassandraSnapshot.scala
@@ -2,9 +2,11 @@ package com.gvolpe.cluster.persistence
 
 import akka.persistence.cassandra.PluginConfiguration
 import akka.persistence.cassandra.snapshot.CassandraSnapshotStore
+import com.typesafe.config.Config
 
 class ShardingCoordinatorCassandraSnapshot extends CassandraSnapshotStore with ShardingSnapshotPluginConfiguration
 
 trait ShardingSnapshotPluginConfiguration extends PluginConfiguration {
-  override def configurationKey: String = "sharding-snapshot-store"
+  override def pluginConfig(systemConfig: Config): Config = systemConfig.getConfig("sharding-snapshot-store")
+    .withFallback(systemConfig.getConfig("cassandra-snapshot-store"))
 }
